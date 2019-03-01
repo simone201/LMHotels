6e0f5b79816fbf97f979c3ff4c1a7cc228a9b4ac
Simone Renzo <simone201@gmail.com>
2019-03-01 10:39:20 +0100

    Adding share button in hotel card

--- app/src/main/java/it/simonerenzo/lmhotels/ui/items/HotelItem.kt
+++ app/src/main/java/it/simonerenzo/lmhotels/ui/items/HotelItem.kt
@@ -1,12 +1,17 @@
 package it.simonerenzo.lmhotels.ui.items
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.view.View
+import androidx.appcompat.widget.AppCompatImageButton
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.appcompat.widget.AppCompatTextView
 import com.bumptech.glide.Glide
+import com.jakewharton.rxbinding2.view.RxView
 import com.mikepenz.fastadapter.FastAdapter
 import com.mikepenz.fastadapter.items.AbstractItem
 import com.mikepenz.materialize.holder.StringHolder
+import com.trello.rxlifecycle3.kotlin.bindToLifecycle
 import hyogeun.github.com.colorratingbarlib.ColorRatingBar
 import it.simonerenzo.lmhotels.R
 import it.simonerenzo.lmhotels.net.models.Hotel
@@ -29,7 +34,9 @@ class HotelItem(val hotelItem: Hotel)
         private val name by bindView<AppCompatTextView>(R.id.hotelName)
         private val stars by bindView<ColorRatingBar>(R.id.hotelStars)
         private val address by bindView<AppCompatTextView>(R.id.hotelAddress)
+        private val share by bindView<AppCompatImageButton>(R.id.hotelShareBtn)
 
+        @SuppressLint("CheckResult")
         override fun bindView(item: HotelItem, payloads: List<Any>) {
             val hotel = item.hotelItem
 
@@ -47,6 +54,25 @@ class HotelItem(val hotelItem: Hotel)
             StringHolder.applyToOrHide(StringHolder("${hotel.location.address} - ${hotel.location.city}"), address)
 
             stars.rating = hotel.stars.toFloat()
+
+            RxView.clicks(share)
+                .bindToLifecycle(itemView)
+                .subscribe {
+                    val shareTemplate = itemView.resources.getString(R.string.share_hotel_template)
+                    val sendIntent: Intent = Intent().apply {
+                        action = Intent.ACTION_SEND
+                        putExtra(Intent.EXTRA_TEXT, String.format(shareTemplate,
+                            hotel.name,
+                            hotel.stars,
+                            hotel.location.address,
+                            hotel.location.city))
+                        type = "text/plain"
+                    }
+
+                    itemView.context.startActivity(
+                        Intent.createChooser(sendIntent,
+                            itemView.resources.getText(R.string.action_send_to)))
+                }
         }
 
         override fun unbindView(item: HotelItem) {
@@ -54,6 +80,7 @@ class HotelItem(val hotelItem: Hotel)
             name.text = null
             stars.rating = 0f
             address.text = null
+            share.setOnClickListener(null)
         }
 
     }
